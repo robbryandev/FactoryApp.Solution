4deabd3fdce85643b2c3bd872525c2054a356a5e
robbryandev <robbryandev@gmail.com>
2023-01-06 12:49:26 -0800

    Add linking machines to engineers

--- Factory/Controllers/EngineerController.cs
+++ Factory/Controllers/EngineerController.cs
@@ -17,6 +17,38 @@ namespace Factory.Controllers
       return View(allEngineers);
     }
 
+    [HttpGet("/engineer/{id}")]
+    public ActionResult View(int id) {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(eng => eng.engineer_id == id);
+      List<Machine> machines = new List<Machine>{};
+      List<EngineerMachine> matchList = _db.EngineerMachines.Where(engmac => engmac.engineer_id == id).ToList();
+      List<Machine> matches = new List<Machine>{};
+      foreach (EngineerMachine engmac in matchList) {
+        Machine thisMachine = _db.Machines.FirstOrDefault(mac => mac.machine_id == engmac.machine_id);
+        matches.Add(thisMachine);
+      }
+      foreach (Machine mac in _db.Machines.ToList()) {
+        if (matches.Contains(mac) == false) {
+          machines.Add(mac);
+        }
+      }
+      ViewBag.matches = matches;
+      ViewBag.machines = machines;
+      return View(thisEngineer);
+    }
+
+    [HttpPost("/engineer/{id}/add")]
+    public ActionResult ViewMachine(int id, string machine) {
+      EngineerMachine newMatch = new EngineerMachine();
+      newMatch.engineer_id = id;
+      newMatch.machine_id = int.Parse(machine);
+      if (_db.EngineerMachines.Contains(newMatch) == false) {
+        _db.EngineerMachines.Add(newMatch);
+        _db.SaveChanges();
+      }
+      return Redirect($"/engineer/{id}");
+    }
+
     [HttpGet("/engineer/add")]
     public ActionResult Add() {
       return View();
